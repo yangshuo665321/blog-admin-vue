07ca7282b5ba841e94496187738051a4d71c443f
yangshuo <ys951207@163.com>
2020-09-18 17:13:30 +0800

    完成新增页面的编写

--- src/views/blog/about/about-add.vue
+++ src/views/blog/about/about-add.vue
@@ -1,11 +1,49 @@
 <template>
   <div>
-    添加关于
+    <el-form ref="form" :model="about" label-width="80px" size="mini">
+      <el-form-item label="标题">
+        <el-input v-model="about.aboutTitle" />
+      </el-form-item>
+      <el-form-item label="内容">
+        <tinymce v-model="about.aboutContent" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" size="mini" @click="onSubmit">提交</el-button>
+        <el-button size="mini">取消</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
-export default {
+// 引入api
+import aboutApi from '@/api/about'
+// 引入Tinymce
+import Tinymce from '@/components/Tinymce/index'
 
+export default {
+  components: {
+    Tinymce
+  },
+  data() {
+    return {
+      about: {
+        aboutId: '',
+        aboutTitle: '',
+        aboutImage: null,
+        aboutContent: null,
+        aboutRemark: null
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      aboutApi.save(this.about).then(res => {
+        this.$message.success(res.msg)
+        this.$emit('closeAddDialog')
+        this.$emit('getByPage')
+      })
+    }
+  }
 }
 </script>
