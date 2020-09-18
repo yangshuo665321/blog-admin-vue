69de1c5b0b56b36b99f3dc1fdb085afb0c862baf
yangshuo <ys951207@163.com>
2020-09-18 17:22:43 +0800

    修改页面完成

--- src/views/blog/about/about-update.vue
+++ src/views/blog/about/about-update.vue
@@ -1,11 +1,49 @@
 <template>
   <div>
-    修改关于
+    <el-form ref="form" :model="about" label-width="80px" size="mini">
+      <el-form-item label="标题">
+        <el-input v-model="about.aboutTitle" />
+      </el-form-item>
+      <el-form-item label="内容">
+        <tinymce v-model="about.aboutContent" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" size="mini" @click="onSubmit">提交</el-button>
+        <el-button size="mini">取消</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
-export default {
+// 引入api
+import aboutApi from '@/api/about'
+// 引入Tinymce
+import Tinymce from '@/components/Tinymce/index'
 
+export default {
+  components: {
+    Tinymce
+  },
+  props: { // 接收从父组件传递来的数据
+    about: {
+      type: Object,
+      default: null
+    }
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    onSubmit() {
+      aboutApi.update(this.about).then(res => {
+        this.$message.success(res.msg)
+        this.$emit('closeUpdateDialog')
+        this.$emit('getByPage')
+      })
+    }
+  }
 }
 </script>
